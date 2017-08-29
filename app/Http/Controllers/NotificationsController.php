0a1be157a481d535ec6a319055b3c6442995941c
kabelo2ka <kabelotooka@gmail.com>
2017-08-29 02:50:47 +0200

    User Notifications wip

---
+++ app/Http/Controllers/NotificationsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\User;
+use Illuminate\Http\Request;
+
+class NotificationsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $notifications = $request->user()->notifications->take(6);
+        $formatted_notifications = [];
+        foreach ($notifications as $notification){
+            // If user commented on song
+            if($notification->type === 'App\Notifications\UserCommented'){
+                $data = $notification->data;
+                $comment = Comment::whereId($data['comment_id'])->with(['author', 'song'])->firstOrFail();
+                $formatted_notifications[] = [
+                    'comment' => $comment,
+                    'author' => $comment['author'],
+                    'song' => $comment['song'],
+                ];
+            }
+        }
+        return response()->json(['data'=>$formatted_notifications], 200);
+    }
+}
